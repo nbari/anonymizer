c9335dd2f55b261229f0934ac5239e1ca8e4c475
nbari <nbari@tequila.io>
2017-08-19 12:58:36 +0200

    update

--- main.go
+++ main.go
@@ -3,36 +3,49 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"net/http/httputil"
+	"time"
 
 	"github.com/nbari/violetear"
 	"github.com/nbari/violetear/middleware"
 )
 
-//func Proxy(w http.ResponseWriter, r *http.Request) *httputil.ReverseProxy {
-func Proxy() *httputil.ReverseProxy {
-	// TODO
-	proxy := &httputil.ReverseProxy{}
-	return proxy
+func Proxy(w http.ResponseWriter, r *http.Request) {
+	director := func(req *http.Request) {
+		req = r
+		req.URL.Scheme = "https"
+		req.URL.Host = r.Host
+	}
+	proxy := &httputil.ReverseProxy{Director: director}
+	proxy.ServeHTTP(w, r)
 }
 
-func randomizer(next http.Handler) http.Handler {
+func start(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// for https
 		if r.Method == "CONNECT" {
 			// TODO
 			fmt.Printf("r.Method = %+v\n", r.Method)
 			return
 		}
-		if r.Method == "GET" {
-			// TODO
-			fmt.Printf("r.Method = %+v\n", r.Method)
-			return
-		}
 		if !r.URL.IsAbs() {
-			fmt.Printf("r.URL = %+v\n", r.URL)
+			fmt.Println("export http_proxy=localhost:8080")
 			return
 		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func randomizer(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userAgents := []string{"foo", "bar", "todo"}
+		// Select a random endpoint
+		rand.Seed(time.Now().UnixNano())
+		i := rand.Intn(len(userAgents))
+		ua := userAgents[i]
+		r.Header.Set("User-Agent", ua)
 		next.ServeHTTP(w, r)
 	})
 }
@@ -40,6 +53,6 @@ func randomizer(next http.Handler) http.Handler {
 func main() {
 	router := violetear.New()
 	router.LogRequests = true
-	router.Handle("*", middleware.New(randomizer).Then(Proxy()))
+	router.Handle("*", middleware.New(start, randomizer).ThenFunc(Proxy))
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
