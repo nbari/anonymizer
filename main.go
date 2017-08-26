16bde389b6007e13d929f40dab9e64281434f85e
nbari <nbari@tequila.io>
2017-08-26 12:51:50 +0200

    modified:   main.go

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 
 func Proxy(w http.ResponseWriter, r *http.Request) {
 	director := func(req *http.Request) {
+		fmt.Printf("req = %+v\n", req.URL)
 		req = r
 		req.URL.Scheme = "https"
 		req.URL.Host = r.Host
